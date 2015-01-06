a539ba61649604312a66ecdded7b7e93d5aeaeb2
jsvenancio <obloggueiro@gmail.com>
2015-01-06 03:05:34 +0000

    Atualização

---
+++ pt-pt/admin_ranks.php
@@ -0,0 +1,27 @@
+<?php
+
+// Definições de linguagem utilizada em todos os arquivos da administração
+$lang_admin_ranks = array(
+
+'Rank head'						=>	'Adicionar, editar ou remover categorias',
+'Rank'							=>	'Categoria',
+'Rank added'					=>	'Categoria adicionada.',
+'Title message'					=>	'É necessário inserir um título de categoria.',
+'Min posts message'				=>	'Mensagens mínimas devem ser um valor inteiro positivo.',
+'Min posts occupied message'	=>	'Já existe uma categoria com um valor mínimo de mensagens %s.',
+'Rank updated'					=>	'Categoria atualizada.',
+'Rank removed'					=>	'Categoria removida.',
+'Add rank intro'				=>	'Insira uma Categoria e o número mínimo de publicações que um usuário tem que ter para adquirir o rank. Diferentes categorias não podem ter o mesmo valor para os posts mínimos. Se um título é definido para um usuário, o título será exibido em vez de qualquer categoria. Para que isso tenha qualquer efeito "<strong>usuário classifica</strong>" deve estar habilitado %s.',
+'Add rank'						=>	'Adicionar categoria',
+'Add rank legend'				=>	'Novos detalhes de classificação',
+'Rank title label'				=>	'Título da Categoria',
+'Min posts label'				=>	'Publicações minimas',
+'Existing ranks legend'			=>	'Editar ou remover categorias de usuários existentes',
+'No ranks'						=>	'Sem categorias na lista.',
+'Update'						=>	'Atualizar',
+'Remove'						=>	'Remover',
+'New rank'						=>	'Adicionar nova categoria',
+'Existing rank'					=>	'Categoria existente',
+
+);
+
---
+++ pt-pt/mail_templates/form_email.tpl
@@ -0,0 +1,13 @@
+Assunto: <mail_subject>
+
+<sender> de <board_title> enviou-lhe uma mensagem. Você pode responder aos <sender> respondendo a este e-mail.
+
+A mensagem diz o seguinte:
+-----------------------------------------------------------------------
+
+<mail_message>
+
+-----------------------------------------------------------------------
+
+--
+<board_mailer>
